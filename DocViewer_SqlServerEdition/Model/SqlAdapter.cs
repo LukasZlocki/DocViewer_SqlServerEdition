535bb172d0c23317bd7c7545e884918ccef26885
lzlocki <lzlocki@poczta.fm>
2020-02-15 10:40:11 +0100

    add SqlAdapter and UserSettings models

---
+++ DocViewer_SqlServerEdition/Model/SqlAdapter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocViewer_SqlServerEdition.Model
+{    
+    class SqlAdapter
+    {
+        // ToDo : Code object to handle with user settings
+        static string USER_SETTINGS_CONNECTION_STRING = "";
+        
+
+        // ToDo : Code connection to database and extaction of proper document
+
+    }
+}
---
+++ DocViewer_SqlServerEdition/Model/UserSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocViewer_SqlServerEdition.Model
+{
+    class UserSettings
+    {
+        public bool IsLoadingStation { get; set; }
+        public string  ResourcesPath { get; set; }
+        public string SqlConnectionString { get; set; }
+    }
+}
