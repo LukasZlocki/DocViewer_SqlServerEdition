6a68bb04ac7782617f25372715ddc1150e796286
lzlocki <lzlocki@poczta.fm>
2020-02-15 12:13:25 +0100

    add IOSettingsAdapter model

---
+++ DocViewer_SqlServerEdition/Model/IOSettingsAdapter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocViewer_SqlServerEdition.Model
+{
+    class IOSettingsAdapter
+    {
+        // ToDo : Code writing / loading user settings
+    }
+}
