255e3d69d1a0dee9441265795aec708ed2de22ae
lzlocki <lzlocki@poczta.fm>
2020-02-15 08:27:11 +0100

    Create methods for find instruction user button

--- DocViewer_SqlServerEdition/MainWindow.xaml.cs
+++ DocViewer_SqlServerEdition/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DocViewer_SqlServerEdition.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -27,5 +28,39 @@ namespace DocViewer_SqlServerEdition
         {
             InitializeComponent();
         }
+
+        #region BUTTONS
+        private void btnShow_Click(object sender, RoutedEventArgs e)
+        {
+            string _partNumber = "";
+
+            Document Instruction = new Document();
+            _partNumber = txtBoxID.Text;
+
+            // Load instruction from database
+            LoadDocuments(_partNumber, ref Instruction);
+
+            // Show instruction on user screen
+            ShowInstructionOnScreen();
+            
+        }
+
+        #endregion
+
+
+        // Load instruction from database
+        private void LoadDocuments(string partNb, ref Document instruction)
+        {
+            // ToDo : code loaing instruction from sql server
+
+        }
+
+        // Show instruction on user screen
+        private void ShowInstructionOnScreen()
+        {
+            // ToDo : code showing instruction on user screen
+
+        }
+
     }
 }
